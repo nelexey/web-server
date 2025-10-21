9802f73e45ca2b4927d87282d2d34b63f03f9d8d
nelexey <codenamed.asyncz@gmail.com>
2025-10-22 00:14:02 +0300

    Implement BaseClient for standardized API calls

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import asyncio
-from web import Server
-import aiohttp
+from web import Server, BaseClient
 
 async def main() -> None:
     main_server = Server('localhost', 8080)
     
     await main_server.start()
+
+    wttr = BaseClient('https://wttr.in/')
+
+    print(await wttr.request('Москва', data={'format': 3}))
 
     while True:
         await asyncio.sleep(3600)
